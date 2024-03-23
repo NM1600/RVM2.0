3dc6bc7c90e2290391182ecfc3fcdd095f6df800
shish <shish@SHISHIII>
2024-03-23 12:58:14 +0800

    added css form to lessen libog added fonts style poppins

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,5 +1,8 @@
 <html>
     <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <title>Owner Dashboard</title>
         <style>
             body {
@@ -90,7 +93,7 @@
             <div class="img_container">
                 <img src="profile.png" alt="Profile" class="profile_img">
             </div>
-            <h1>Reycle and Earn</h1>
+            <img src="logoEarn.png" alt="Profile" class="profile_img">
             <p>Welcome Administrator!</p>
             <div class="dashboard_container">
                 <input type="submit" value="Scan QR" class="ScanQRBtn" name="ScanQR_Btn" formaction="QR.php" target="_blank">
--- form0.php
+++ form0.php
@@ -1,81 +1,17 @@
 <html>
 <head>
-    <title>Recycle Earn</title>
-    <style>
-        body{
-            background: linear-gradient(to bottom, #053d02, #187f13, #18d22e, #60ff04);
-            background-size: cover;
-            margin: 0;
-            padding: 0;
-        }
-        .container{
-            text-align: center;
-        }
-        form{
-            background: rgba(255, 255, 255, 0);
-            width: 550px;
-            height: 580px;
-            padding: 75px 50px;
-            position:absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%,-50%);
-        }
-        h1{
-            text-align: center;
-            margin-bottom: 65px;
-            color: yellow;
-            font-size: 45px;
-            font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-            font-weight:400;
-        }
-        p{
-            text-align: center;
-            margin-bottom: 5px;
-            color: #ffff;
-            font-size: 18px;
-            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
-        .studentBtn{
-            height: 45px;
-            width: 60%;
-            margin-top: 15px;
-            border: none;
-            outline: none;
-            background: #053d02;
-            background-size: 200%;
-            color: white;
-            font-size: 16px;
-        }
-        .studentBtn:hover {
-            background-color: #0bd830; /* New background color when hovering */
-            background-position: center;
-            font-size: 16px;
-            color: black;
-
-        }
-        .adminBtn{
-            height: 45px;
-            width: 60%;
-            margin-top: 15px;
-            border: none;
-            outline: none;
-            background: #053d02;
-            background-size: 200%;
-            color: white;
-            font-size: 16px;
-        }
-        .adminBtn:hover {
-            background-color: #0bd830; /* New background color when hovering */
-            background-position: center;
-            font-size: 16px;
-            color: black;
-        }
-    </style>
+     <link rel="stylesheet" href="form0.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Welcome to Recycle Earn</title>
 </head>
 <body>
-    <form>
-        <h1>Recycle and Earn</h1>
+        <div class = "container-form">
+            <form class = "form">
+        <div class = "logo-container">
+            <img src="logoEarn.png" class = "logo" alt="logo">
+    </div>
         <p>Select your Role:</p>
         <div class="container">
             <input type="submit" value="Student User" class="studentBtn" name="student_Btn" formaction="studentLogin.php" target="_blank">
