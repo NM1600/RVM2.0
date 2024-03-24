b08e6698ad98c30493493ba56f266fba2b2095ce
shish <shish@SHISHIII>
2024-03-24 13:54:23 +0800

    new

--- adminDashboard.php
+++ adminDashboard.php
@@ -4,7 +4,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        <title>Owner Dashboard</title>
+        <title>Welcome Administrator!</title>
         </style>
     </head>
     <body>
@@ -17,7 +17,7 @@
 </div>
             <div class="dashboard_container">
                 <input type="submit" value="Scanned QR History" class="validateBtn" name="Validate_Btn" formaction="adminValidateReward.php" target="_blank">
-                <input type="submit" value="Scan QR" class="scanQR" name="scanQR_Btn" formaction="scanQR.php" target="_blank">
+                <button type="submit" class="scanQR" onclick="redirectToForm()">Scan QR</button>
                 <button type="button" class="LogoutBtn" onclick="redirectToForm()">Logout</button>
             </div>
         </form>
@@ -25,6 +25,10 @@
     function redirectToForm() {
         window.location.href = "form0.php";
     }
+    function redirectToForm() {
+        window.location.href = "scanQR.php";
+    }
+
     </script>
     </body>
 </html>
