ae23e658d97c071855e038083e020cb6911f5ab1
Nestle Ann Mercado <119910327+NM1600@users.noreply.github.com>
2024-03-25 11:11:03 +0800

    Add files via upload

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,37 +1,137 @@
 <html>
     <head>
-    <link rel="stylesheet" href="adminDashboard.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        <title>Welcome Administrator!</title>
+        <title>Owner Dashboard</title>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                background: linear-gradient(to bottom, #053d02, #187f13, #18d22e, #60ff04);
+            }
+            form{
+                background: rgba(255, 255, 255, 0); /* RGBA color with 80% opacity */
+                width: 550px;
+                height: none;
+                padding: 75px 50px;
+                position:absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%,-50%);
+            }
+            .img_container {
+                text-align: center;
+                width: 200px; /* Adjust the width of the container as needed */
+                height: 200px; /* Adjust the height of the container as needed */
+                margin: 0 auto; /* Center the container horizontally */
+                margin-bottom: 5px; /* Adjust margin as needed */
+            }
+            .img_container img {
+                max-width: 100%; /* Set the maximum width of the image to 100% of the container */
+                max-height: 100%; /* Set the maximum height of the image to 100% of the container */
+            }
+            h1{
+                    text-align: center;
+                    margin-bottom: 65px;
+                    color: yellow;
+                    font-size: 45px;
+                    font-family: cursive;                
+                    font-weight: 400;
+            }
+            p{
+                text-align: left;
+                margin-bottom: 5px;
+                color: white;
+                font-size: 18px;
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+            }
+            .dashboard_container {
+                text-align: center;
+            }
+            .validateBtn{
+                height: 45px;
+                width: 100%;
+                margin-top: 15px;
+                border: none;
+                outline: none;
+                background: #053d02;
+                background-size: 200%;
+                color: white;
+                font-size: 16px;
+            }
+            .validateBtn:hover {
+                background-color: #0bd830; /* New background color when hovering */
+                background-position: center;
+                font-size: 16px;
+                color: black;
+            }
+            .scanQR{
+                height: 45px;
+                width: 100%;
+                margin-top: 15px;
+                border: none;
+                outline: none;
+                background: #053d02;
+                background-size: 200%;
+                color: white;
+                font-size: 16px;
+            }
+            .scanQR:hover {
+                background-color: #0bd830; /* New background color when hovering */
+                background-position: center;
+                font-size: 16px;
+                color: black;
+            }
+            .addUser{
+                height: 45px;
+                width: 100%;
+                margin-top: 15px;
+                border: none;
+                outline: none;
+                background: #053d02;
+                background-size: 200%;
+                color: white;
+                font-size: 16px;
+            }
+            .addUser:hover {
+                background-color: #0bd830; /* New background color when hovering */
+                background-position: center;
+                font-size: 16px;
+                color: black;
+            }
+            .LogoutBtn{
+                height: 45px;
+                width: 100%;
+                margin-top: 15px;
+                border: none;
+                outline: none;
+                background: #053d02;
+                background-size: 200%;
+                color: white;
+                font-size: 16px;
+            }
+            .LogoutBtn:hover {
+                background-color: #676767; /* New background color when hovering */
+                background-position: center;
+                font-size: 16px;
+                color: black;
+            }
         </style>
     </head>
     <body>
-        <div class = "card">
-        <div class = "logo-container">
-            <img src="logoEarn.png" class="logo" alt="logo">
-            <p>Welcome Administrator!</p>
-        <div class="img_container">
+        <form>
+            <div class="img_container">
                 <img src="profile.png" alt="Profile" class="profile_img">
-        </div>
-        <div class="dashboard_container">
-                <button type="submit" class="historyBtn" onclick="redirectTohistory()">Scan QR History</button>
-                <button type="submit" class="scanQR" onclick="redirectToscanQR()">Scan QR</button>
-                <button type="button" class="LogoutBtn" onclick="redirectToForm()">Logout</button>
-        </div>
+            </div>
+            <h1>Reycle and Earn</h1>
+            <p>Welcome Administrator!</p>
+            <div class="dashboard_container">
+                <input type="submit" value="Scanned QR History" class="validateBtn" name="Validate_Btn" formaction="adminValidateReward.php" target="_blank">
+                <input type="submit" value="Scan QR" class="scanQR" name="scanQR_Btn" formaction="scanQR.php" target="_blank">
+                <input type="submit" value="Add New User" class="addUser" name="addUser_Btn" formaction="addNewUser.php" target="_blank">
+                <input type="submit" value="Logout" class="LogoutBtn" name="Logout_Btn" formaction="form0.php" target="_blank">
+            </div>
         </form>
-
-        <script>
-    function redirectToForm() {
-        window.location.href = "index.php";
-    }
-    function redirectToscanQR() {
-        window.location.href = "scanQR.php";
-    }
-    function redirectTohistory() {
-        window.location.href = "adminValidateReward.php";
-    }
-    </script>
     </body>
 </html>
